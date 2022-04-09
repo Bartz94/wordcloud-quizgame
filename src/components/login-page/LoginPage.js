ce88a7577aaeb6e16f243c2a673b70c782c12ced
Bartosz Czyzewski <bart94946@gmail.com>
2022-04-09 16:41:38 +0200

    added UserContextProvider, setting user name to a state

--- src/components/login-page/LoginPage.js
+++ src/components/login-page/LoginPage.js
@@ -1,16 +1,26 @@
 import { NavLink } from 'react-router-dom';
-import Typography from '@mui/material/Typography'
+import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import styled from 'styled-components';
 import { useMediaQuery } from '@mui/material';
+import { useUserContext } from '../../services/UserContextProvider';
 
 export const LoginPage = () => {
-    const isActive = useMediaQuery("(min-width: 1000px)")
+    const isActive = useMediaQuery("(min-width: 1000px)");
+    const { setName } = useUserContext();
+
+    const handleSetName = (event) => {
+        setName(event.target.value);
+    };
+
     return (
         <>
             <Typography className='page-title' variant={isActive ? 'h1' : 'h2'}>WordCloud game</Typography>
-            <TextField label="Enter your nickname here..." fullWidth margin='normal' />
+            <TextField
+                fullWidth
+                label="Enter your nickname here..."
+                margin='normal'
+                onChange={handleSetName} />
             <Button sx={{
                 textTransform: 'lowercase',
                 padding: '8px 60px',
@@ -22,6 +32,5 @@ export const LoginPage = () => {
                 play
             </Button>
         </>
-
-    )
-}
+    );
+};
