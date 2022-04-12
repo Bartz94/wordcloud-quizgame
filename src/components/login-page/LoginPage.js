f5c1e2c1d917a0cd840c00df6096db62add9afd5
Bartosz Czyzewski <bart94946@gmail.com>
2022-04-12 16:13:57 +0200

    added validation

--- src/components/login-page/LoginPage.js
+++ src/components/login-page/LoginPage.js
@@ -1,16 +1,32 @@
-import { NavLink } from 'react-router-dom';
+import { useNavigate, Link } from "react-router-dom";
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
+import { Alert } from '@mui/material';
 import Button from '@mui/material/Button';
 import { useMediaQuery } from '@mui/material';
 import { useUserContext } from '../../services/UserContextProvider';
+import { useState } from 'react';
+
+
 
 export const LoginPage = () => {
     const isActive = useMediaQuery("(min-width: 1000px)");
-    const { setName } = useUserContext();
+    const { name, setName } = useUserContext();
+    const [errors, setError] = useState(false);
+
 
     const handleSetName = (event) => {
         setName(event.target.value);
+    };
+
+    const navigate = useNavigate();
+    const handlePlayButton = (event) => {
+        if (name) {
+            navigate(`/question-page`)
+        }
+        else {
+            setError(true);
+        }
     };
 
     return (
@@ -20,17 +36,28 @@ export const LoginPage = () => {
                 fullWidth
                 label="Enter your nickname here..."
                 margin='normal'
-                onChange={handleSetName} />
+                onChange={handleSetName}
+                error={errors}
+                value={''}
+            />
             <Button sx={{
                 textTransform: 'lowercase',
                 padding: '8px 60px',
                 fontSize: '30px',
                 fontWeight: '400',
             }}
-                variant="outlined"
-                component={NavLink} to="/question-page">
+                onClick={handlePlayButton}
+                variant="outlined">
                 play
             </Button>
+            {errors ?
+                <>
+                    <Alert sx={{ m: 3 }} variant="outlined" severity="error">
+                        Error — please enter your nickname!
+                    </Alert>
+                </>
+                : ''
+            }
         </>
     );
 };
